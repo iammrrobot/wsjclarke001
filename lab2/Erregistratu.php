091af26f41b2ed9c944fca00de27078e8589cb9e
jclarke001 <julen.a.clarke@gmail.com>
2015-10-03 12:22:17 +0200

    Lab2-ko 2. puntu arte eginda.

---
+++ lab2/Erregistratu.php
@@ -0,0 +1,15 @@
+<?php
+	//phpinfo();
+	// Konexioa sortu
+	$sql = mysql_connect('localhost', 'root', '') or die(mysql_error());
+	// Konexioa egiaztatu
+	mysql_select_db("quiz") or die(mysql_error());
+	$sql="INSERT INTO Erabiltzaile(Izena, Eposta, Pasahitza, Telefonoa, Espezialitatea, Interesak) VALUES ('$_POST[Izenabizenak]', '$_POST[Eposta]', '$_POST[Pasahitza]', '$_POST[Telefonoa]', '$_POST[Espezialitatea]', '$_POST[Interesak]')";
+	if (!mysql_query($sql))
+	{
+		die('Errorea: ' . mysql_error());
+	}
+	echo "Txertatze bat eginda.";
+	mysql_close();
+	echo "<p> <a href='IkusiErabiltzaileak.php'> Erabiltzaileak ikusi </a>";
+?> 
