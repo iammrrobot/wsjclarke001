8b670510ff6810983c4350e3887b66fe7f9c44a4
jclarke001 <julen.a.clarke@gmail.com>
2015-10-23 17:55:39 +0200

    Lab4ko derrigorrezko garapena bukatua.

--- lab4/php/IkusiErabiltzaileak.php
+++ lab4/php/IkusiErabiltzaileak.php
@@ -1,12 +1,12 @@
 <?php
 	// Konexioa sortu
-	$sql = mysql_connect('mysql.hostinger.es', 'u275359965_root', 'dbroot') or die(mysql_error());
+	//$sql = mysql_connect('mysql.hostinger.es', 'u275359965_root', 'dbroot') or die(mysql_error());
 	// Konexioa egiaztatu
-	mysql_select_db("u275359965_quiz") or die(mysql_error());
+	//mysql_select_db("u275359965_quiz") or die(mysql_error());
 	// Konexio lokala sortu
-	//$sql = mysql_connect('localhost', 'root', '') or die(mysql_error());
+	$sql = mysql_connect('localhost', 'root', '') or die(mysql_error());
 	// Konexioa lokala egiaztatu
-	//mysql_select_db("quiz") or die(mysql_error());
+	mysql_select_db("quiz") or die(mysql_error());
 	$sql="SELECT * FROM `Erabiltzaile`";
 	$records = mysql_query($sql);
 	if (! $records)
@@ -15,10 +15,20 @@
 	}
 	mysql_close();
 ?>
-
+<!DOCTYPE html>
 <html>
 	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 		<title>Erabiltzaileak</title>
+		<link rel='stylesheet' type='text/css' href='stylesPWS/style.css'>
+		<link rel='stylesheet' 
+			   type='text/css' 
+			   media='only screen and (min-width: 530px) and (min-device-width: 481px)'
+			   href='stylesPWS/wide.css'>
+		<link rel='stylesheet' 
+			   type='text/css' 
+			   media='only screen and (max-width: 480px)'
+			   href='stylesPWS/smartphone.css'>
 	</head>
 	<body>
 		<table width="800" border="1" cellpadding="1" cellspacing="1">
@@ -52,5 +62,8 @@
 			?>
 			
 		</table>
+		<span>
+			<a href="../layout.html">Hasierako orria</a><br>
+		</span>	
 	</body>
 </html> 
