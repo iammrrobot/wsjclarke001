c7d35f1675497e8660750699eb1f5a1a996facf9
jclarke001 <julen.a.clarke@gmail.com>
2015-10-25 11:20:34 +0100

    Lab4ko garapen lokala bukatua.

--- lab4/php/InsertQuestion.php
+++ lab4/php/InsertQuestion.php
@@ -27,14 +27,31 @@
 		$galdera = $_POST['Galdera'];
 		$erantzuna = $_POST['Erantzuna'];
 		$zailtasuna = $_POST['Zailtasuna'];
+		$ordua = date('Y-m-d H:i:s');
+		$ip = $_SERVER["REMOTE_ADDR"];
+		$mota = "Galdera txertatu";
 		
 		if($hutsa == 1) {
-			$sql="INSERT INTO Galderak(Eposta, Galdera, Erantzuna, Zailtasuna) VALUES ('$eposta', '$galdera', '$erantzuna', '$zailtasuna')";
-		
-			if (!mysql_query($sql))
+			$sqlgaldera="INSERT INTO Galderak(Eposta, Galdera, Erantzuna, Zailtasuna) VALUES ('$eposta', '$galdera', '$erantzuna', '$zailtasuna')";
+			$sqlkonexioa="INSERT INTO Konexioak(Eposta, K_Ordua) VALUES ('$eposta', '$ordua')";
+			if (!mysql_query($sqlgaldera) || !mysql_query($sqlkonexioa))
 			{
 				die('Errorea: ' . mysql_error());
 			}
+			
+			$query="SELECT MAX(K_ID) FROM `Konexioak`";
+			$do = mysql_query($query);
+			$fetch=mysql_fetch_assoc($do);
+			$k_id=$fetch['MAX(K_ID)'];
+			$ip = $_SERVER['REMOTE_ADDR'];
+			
+			$sqlekintza="INSERT INTO Ekintzak(K_ID, Eposta, E_Mota, E_Ordua, IP) VALUES ('$k_id', '$eposta', '$mota', '$ordua', '$ip')";
+			if (! $do || !mysql_query($sqlekintza))
+			{
+				die('Errorea: ' . mysql_error());
+			}
+			
+			
 			echo "Txertatze bat eginda.";
 			mysql_close();
 		}
